3fc25621218e0434c740c648ef30b78f5c204137
anno.ryouya <anno.ryouya@cti.co.jp>
2021-09-03 09:24:20 +0900

    initialcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  useQuery,
+  gql
+} from "@apollo/client";
+import useState from 'react';
+
+const EXCHANGE_RATES = gql`
+query GetExchangeRates($currency: String!) {
+  rates(currency: $currency) {
+    currency
+    rate
+    name
+  }
+}
+`;
+
+let rateData = null;
+let rate = 'USD';
+let refetcha = null;
+
+function useExchangeRates(selectedCurrency) {
+  const currency = selectedCurrency;
+
+  const { loading, error, data, refetch } = useQuery(EXCHANGE_RATES ,{
+    variables: {currency},
+  });
+
+  if (loading) return 'loading';
+  if (error) return 'error';
+
+  console.log(data.rates);
+  refetcha = refetch;
+
+  return data.rates
+}
+
+function ShowRateData(){
+  console.log(rateData);
+
+  if (rateData === 'loading'){
+    return(<p>loading</p>);
+  }else if(rateData === 'error'){
+    return(<p>error</p>);
+  }
+
+  return rateData.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
+
+function onChangeCurrency(e){
+  rate = e.target.value;
+  refetcha();
+}
+
+function SelectCurrency() {
+  const [currency, setCurrency] = useState("USD")
+
+  return(
+    
+      <div>
+          <select name="currency" onChange={(e)=>onChangeCurrency(e)}>
+              <option value='USD'>USD</option>
+              <option value='AOA'>AOA</option>
+              <option value='JPY'>JPY</option>
+          </select>
+      </div>
+  );
+}
 
 function App() {
+  rateData = useExchangeRates(rate);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <SelectCurrency/>
+        <ShowRateData />
+      </div>
     </div>
   );
 }
